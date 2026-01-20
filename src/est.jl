610522cc4d7a9c485b688cf78386e40dd27f2fc9
yuya-nagano-OU <your.email@example.com>
2026-01-20 19:34:45 +0900

    edit test

---
+++ src/est.jl
@@ -0,0 +1,32 @@
+struct BBClModel
+    lmax::Int
+    ell::Vector{Int}
+    cl_lens::Vector{Float64}
+    cl_tens::Vector{Float64}
+    cl_sys::Vector{Float64}
+end
+
+function BBClModel(lmax::Int, cl_sys::Vector{Float64}, reffpath_r0::String, reffpath_r1::String)
+    # Load fiducial power spectra
+    cl_r0 = read_planck_cl_as_TT_EE_BB_TE(reffpath_r0)
+    cl_r1 = read_planck_cl_as_TT_EE_BB_TE(reffpath_r1)
+    # Extract BB component (index 3 for BB)
+    cl_lens = cl_r0.BB
+    cl_tens = cl_r1.BB
+    ell = collect(2:lmax)
+    return BBClModel(lmax, ell, cl_lens[ell.+1], cl_tens[ell.+1], cl_sys[ell.+1])
+end
+
+function read_planck_cl_as_TT_EE_BB_TE(fname; hdu_index=2)
+    FITS(fname) do f
+        hdu = f[hdu_index]  # TableHDU
+        TT = read(hdu, "TEMPERATURE")
+        EE = read(hdu, "GRADIENT")
+        BB = read(hdu, "CURL")
+        TE = read(hdu, "G-T")
+        N = length(TT)
+        ell = collect(0:N-1)
+        return (ell=ell, TT=TT, EE=EE, BB=BB, TE=TE)
+    end
+end
+
