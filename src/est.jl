d58f7b2acf1fb00e40908f3d745dfc9ae474fad3
yuya-nagano-OU <your.email@example.com>
2026-02-04 15:31:45 +0900

    add functions

--- src/est.jl
+++ src/est.jl
@@ -1,10 +1,10 @@
 using FITSIO
 using Plots
 using LaTeXStrings
-using Optim
+#using Optim
 
 
-struct BBClModel
+struct ClBBModel
     lmax::Int
     ell::Vector{Int}
     cl_lens::Vector{Float64}
@@ -12,7 +12,7 @@ struct BBClModel
     cl_sys::Vector{Float64}
 end
 
-function BBClModel(lmax::Int, cl_sys::Vector{Float64}, reffpath_r0::String, reffpath_r1::String)
+function ClBBModel(lmax::Int, cl_sys::Vector{Float64}, reffpath_r0::String, reffpath_r1::String)
     # Load fiducial power spectra
     cl_r0 = read_planck_cl_as_TT_EE_BB_TE(reffpath_r0)
     cl_r1 = read_planck_cl_as_TT_EE_BB_TE(reffpath_r1)
@@ -20,7 +20,17 @@ function BBClModel(lmax::Int, cl_sys::Vector{Float64}, reffpath_r0::String, reff
     cl_lens = cl_r0.BB
     cl_tens = cl_r1.BB
     ell = collect(2:lmax)
-    return BBClModel(lmax, ell, cl_lens[ell.+1], cl_tens[ell.+1], cl_sys[ell.+1])
+    return ClBBModel(lmax, ell, cl_lens[ell.+1], cl_tens[ell.+1], cl_sys[ell.+1])
+end
+
+function ClBBModel(lmax::Int, cl_sys::Vector{Float64}, cl_lens::Vector{Float64}, cl_tens::Vector{Float64})
+    ell = collect(2:lmax)
+    return ClBBModel(lmax, ell, cl_lens[ell.+1], cl_tens[ell.+1], cl_sys[ell.+1])
+end
+
+function ClBBModel(;lmax::Int, cl_sys::Vector{Float64}, cl_lens::Vector{Float64}, cl_tens::Vector{Float64})
+    ell = collect(2:lmax)
+    return ClBBModel(lmax, ell, cl_lens[ell.+1], cl_tens[ell.+1], cl_sys[ell.+1])
 end
 
 function read_planck_cl_as_TT_EE_BB_TE(fname; hdu_index=2)
@@ -36,14 +46,13 @@ function read_planck_cl_as_TT_EE_BB_TE(fname; hdu_index=2)
     end
 end
 
-function r_iterative_estimator(params::BBClModel,; rmin, rmax, rresol, itr = 1)
+function r_iterative_estimator(params::ClBBModel,; rmin, rmax, rresol, itr = 1)
     r_grid = range(rmin, rmax; length = Int(rresol))
     logL = zeros(length(r_grid))
     cl_obs = params.cl_lens .+ params.cl_sys
     ell = params.ell
     Δr = 0.0
     for itr_idx in 1:itr
-        r_grid = range(rmin, rmax; length = Int(rresol))
         for (idx, r) in enumerate(r_grid)
             cl_th = params.cl_lens .+ r .* params.cl_tens
             logL[idx] = _logL(ell, cl_obs, cl_th)
@@ -60,7 +69,7 @@ end
 return sum(-1/2 .* (2 .* ell .+ 1) .* (cl_obs ./ cl_th .+ log.(cl_th) .-((2 .* ell .- 1) ./ (2 .* ell .+ 1)) .* log.(cl_obs)))
 end
 
-function δr_estimator(params::BBClModel,; rmin, rmax, rresol)
+function δr_estimator(params::ClBBModel,; rmin, rmax, rresol)
     r_grid = range(rmin, rmax; length = Int(rresol))
     logL = zeros(length(r_grid))
     cl_obs = params.cl_lens .+ params.cl_sys
